690b4e187784766caeed2586abe914571626f8de
sudipta0311 <43538281+sudipta0311@users.noreply.github.com>
2022-07-30 10:42:36 +0200

    certificate

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import java.security.KeyStore;
+import java.security.cert.Certificate;
+import java.security.PrivateKey;
 
 @SpringBootApplication
 @RestController
@@ -15,6 +20,15 @@ public class DemoApplication {
 
 	@RequestMapping("/")
 	String sayHello() {
-		return "Hello World! update value1";
+		try {
+		KeyStore ks = KeyStore.getInstance("Windows-MY");
+        ks.load(null, null); 
+        Certificate cert = ks.getCertificate("contoso.net");
+        PrivateKey privKey = (PrivateKey) ks.getKey("contoso.net", ("1234").toCharArray());
+		return "Hello World! update value11";
+		} catch (Exception e)
+		{
+		return "Error in certificate ");
+		}
 	}
 }
